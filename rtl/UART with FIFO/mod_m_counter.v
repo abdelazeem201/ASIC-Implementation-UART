e960a259a991ceb61d6f115665a38c827aa91d5e
Ahmed Abdelazeem <58098260+abdelazeem201@users.noreply.github.com>
2022-01-04 10:00:43 +0200

    Add files via upload

---
+++ rtl/UART with FIFO/mod_m_counter.v
@@ -0,0 +1,42 @@
+// --------------------------------------------------------------------
+// >>>>>>>>>>>>>>>>>>>>>>>>> COPYRIGHT NOTICE <<<<<<<<<<<<<<<<<<<<<<<<<
+// --------------------------------------------------------------------
+// Author: Ahmed Abdelazeem
+// Github: https://github.com/abdelazeem201
+// Email: ahmed_abdelazeem@outlook.com
+// Description: mod_m_counter Module
+// Dependencies: 
+// Since: 2021-12-18 15:24:08
+// LastEditors: ahmed abdelazeem
+// LastEditTime: 2021-12-18 15:24:08
+// ********************************************************************
+module mod_m_counter
+   #(
+    parameter N=8, // number of bits in counter
+              M=163 // mod-M
+   )
+   (
+    input wire clk, reset,
+    output wire max_tick,
+    output wire [N-1:0] q
+   );
+
+   //signal declaration
+   reg [N-1:0] r_reg;
+   wire [N-1:0] r_next;
+
+   // body
+   // register
+   always @(posedge clk, posedge reset)
+      if (reset)
+         r_reg <= 0;
+      else
+         r_reg <= r_next;
+
+   // next-state logic
+   assign r_next = (r_reg==(M-1)) ? 0 : r_reg + 1;
+   // output logic
+   assign q = r_reg;
+   assign max_tick = (r_reg==(M-1)) ? 1'b1 : 1'b0;
+
+endmodule
